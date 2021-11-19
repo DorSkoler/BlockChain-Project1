e781beb0d3f9f8e5eccfba0de63fbf6671556025
Dor Skoler <dorskoler@gmail.com>
2021-11-19 12:50:02 +0200

    m

--- src/blockchain.js
+++ src/blockchain.js
@@ -130,7 +130,7 @@ class Blockchain {
     this.chain = [this.createGenesisBlock()];
     this.difficulty = 2;
     this.pendingTransactions = [];
-    this.miningReward = 150;
+    this.miningReward = 100;
   }
 
   /**
--- src/bloomfiltersearch.js
+++ src/bloomfiltersearch.js
@@ -1,34 +1,17 @@
-const names = ['Abhin', 'Pai', 'dor', 'meirav', 'chair', 'table', 'Achilles', 'Abubakar', 'Absalon', 'Abie', 'Abdullatif', 'Abdulla', 'Abbye', 'Abbagail', 'Abayomi', 'Aarthi', 'Aarik', 'Aamil', 'Aamanda']; // n names
-const noOfHashFunction = 6; // number of hash functions
+const { BloomFilter } = require('bloom-filters');
+const filter = new BloomFilter(40000, 6);
 
-const storage = Array(Math.pow(2, 22) - 1).fill(0); // Bllom filter bit
-
-const hash = (key) => {
-  const hashNumbers = [];
-  for (let i = 1; i <= noOfHashFunction; i++) {
-    hashNumbers.push(
-      Math.abs(
-        key.split('').reduce((a, b) => ((a << i) - a + b.charCodeAt(0)) | 0, 0)
-      )
-    );
-  }
-  return hashNumbers;
+const readFromStorage = () => {
+  var fs = require('fs-extra');
+  var text = fs.readFileSync('./names.txt', 'utf8');
+  const namesFromTxt = text.split('\r\n');
+  namesFromTxt.forEach((name) => {
+    filter.add(name);
+  });
 };
 
-// Initilizing bloom filter bit for a hash index
-names.forEach((name) => {
-  const indexes = hash(name);
-  indexes.forEach((index) => (storage[index] = 1));
-});
-
-const result = [];
-const isValueContainInBloom = (searchString) => {
-  const hashes = hash(searchString);
-  const result = hashes.filter((index) => !storage[index]);
-  return !(result.length > 0);
-};
-
-
-console.log(isValueContainInBloom('Pai'));
-console.log(isValueContainInBloom('dor'));
-console.log(isValueContainInBloom('ariel'));
+filter.add('bob');
+readFromStorage();
+console.log(filter.has('bob')); // output: true
+console.log(filter.has('daniel')); // output: false
+console.log(filter.rate());
