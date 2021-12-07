c7f56a52934cf40dbcfb11b911912daaf2e572fe
MeiravGo <go.meirav@gmail.com>
2021-12-07 10:55:31 +0200

    hi

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+//import {mempool} from './transactions.js';
 const { Blockchain, Transaction,SPV } = require('./blockchain');
 const SHA256 = require('crypto-js/sha256')
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
+const {memPool} = require('./transactions');
+
 
 
 // Your private key goes here
@@ -12,31 +15,34 @@ const myWalletAddress = myKey.getPublic('hex');
 console.log(myWalletAddress);
 // Create new instance of Blockchain class
 const savjeeCoin = new Blockchain();
+savjeeCoin.pendingTransactions=memPool();
+console.log(savjeeCoin.pendingTransactions);
 
 // Mine first block
 savjeeCoin.minePendingTransactions(myWalletAddress);
 
-// Create a transaction & sign it with your key
+
+// // Create a transaction & sign it with your key
 const tx1 = new Transaction(myWalletAddress, 'address2', 100);
 tx1.signTransaction(myKey);
 savjeeCoin.addTransaction(tx1);
 console.log(tx1);
 
-// Mine block
+// // Mine block
 savjeeCoin.minePendingTransactions(myWalletAddress);
 
-// Create second transaction
+// // Create second transaction
 const tx2 = new Transaction(myWalletAddress, 'address1', 50);
 tx2.signTransaction(myKey);
 savjeeCoin.addTransaction(tx2);
 
-// Mine block
+// // Mine block
 savjeeCoin.minePendingTransactions(myWalletAddress);
 
 console.log();
 console.log(`Balance of xavier is ${savjeeCoin.getBalanceOfAddress(myWalletAddress)}`);
 
-// Check if the chain is valid
+// // Check if the chain is valid
 
 console.log();
 console.log('Blockchain valid?', savjeeCoin.isChainValid() ? 'Yes' : 'No');
